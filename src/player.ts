3815c5302b046cc3cc6222ad0247ba614e9f9365
Francois Paillon <francoispaillon@hotmail.fr>
2022-12-17 23:51:40 +0100

    first version of movements

--- src/player.ts
+++ src/player.ts
@@ -1,34 +1,59 @@
 import { Application, Sprite, Container } from "pixi.js";
-import { Keyboard } from "./keyboard";
 
 export class Player extends Container {
   app: Application;
   sprite: Sprite;
+  speed: any;
+  keys: any;
+  acceleration: number;
 
   constructor(app: Application) {
     super();
     this.app = app;
     this.sprite = Sprite.from("frames/knight_f_hit_anim_f0.png");
-    this.x = app.screen.width / 2;
-    this.y = app.screen.height / 2;
+    this.position.x = app.screen.width / 2;
+    this.position.y = app.screen.height / 2;
+    this.speed = {
+      x: 0,
+      y: 0,
+    };
+    this.acceleration = 5;
+    this.keys = {
+      right: {
+        pressed: false,
+      },
+      left: {
+        pressed: false,
+      },
+      up: {
+        pressed: false,
+      },
+      down: {
+        pressed: false,
+      },
+    };
     this.sprite.interactive = true;
     this.addChild(this.sprite);
-    app.ticker.add((delta) => this.gameLoop(delta));
+    app.ticker.add(() => {
+      this.update();
+    });
   }
 
-  gameLoop(delta: number) {
-    this.movement(delta);
+  update() {
+    // this.update();
+    this.position.y += this.speed.y;
+    this.position.x += this.speed.x;
 
-    // Keyboard.update();
-  }
+    if (this.keys.right.pressed) {
+      this.speed.x = this.acceleration;
+    } else if (this.keys.left.pressed) {
+      this.speed.x = -this.acceleration;
+    } else this.speed.x = 0;
 
-  movement(delta: number) {
-    const speed: number = delta * 5;
-
-    if (Keyboard.isKeyDown("ArrowLeft", "KeyA")) this.x -= speed;
-    if (Keyboard.isKeyDown("ArrowRight", "KeyD")) this.x += speed;
-
-    if (Keyboard.isKeyDown("ArrowUp", "KeyW")) this.y -= speed;
-    if (Keyboard.isKeyDown("ArrowDown", "KeyS")) this.y += speed;
+    if (this.keys.up.pressed) {
+      this.speed.y = -this.acceleration;
+    } else if (this.keys.down.pressed) {
+      this.speed.y = this.acceleration;
+    } else this.speed.y = 0;
   }
 }
